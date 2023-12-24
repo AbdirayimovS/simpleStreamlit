aa1ee1b21f9a9cb7a0d6ee57d3b77b163b8f5b53
AbdirayimovS <abdirayimovsardor20@gmail.com>
2023-12-24 11:47:23 +0900

    updating main.py

--- pixmatch.py
+++ pixmatch.py
@@ -5,7 +5,17 @@ import random
 import base64
 import json
 from PIL import Image
+import requests as r
 
 st.set_page_config(page_title = "PixMatch", page_icon="🕹️", layout = "wide", initial_sidebar_state = "expanded")
-
+url = 'https://ntfy.sh/salvador'
 st.title("Simple Web Site: Salvador")
+
+def main(data = "From streamlit"):
+    data = {'message': f"Streamlit {tm.time()} {os.uname()}"}
+    response = r.post(url, data=data)
+    st.toast(response.status_code)
+    
+
+if __name__ == "__main__":
+    main()
